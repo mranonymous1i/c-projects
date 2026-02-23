6438b592a944c13a9432b2b3f5125a50976852fa
mranonymous1i <abd.ahd2006@gmail.com>
2026-02-22 22:23:29 -0500

    Add linked list implementation

---
+++ linked_lists.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node{
+  int data;
+  struct Node *next;
+} Node;
+
+void extract(Node** list, int requested);
+void insert_head(Node** list, int data);
+void insert_tail(Node** list, int data);
+void print_list(Node** list);
+
+int main(void){
+  Node *linkedList = malloc(sizeof(Node));
+  linkedList->data = 10;
+  linkedList->next = NULL;
+
+  Node *empty = NULL;
+
+  print_list(&empty);
+  insert_head(&linkedList, 10);
+  print_list(&linkedList);
+  insert_tail(&linkedList, 6);
+  print_list(&linkedList);
+
+  free(linkedList);
+  return 0;
+}
+
+void extract(Node** list, int requested){
+  
+  Node *current = *list;
+  Node *temp = NULL;
+
+  while(current != NULL){
+    if(current->data == requested){
+      if(temp == NULL){
+        Node* new_list = current->next;
+        free(current);
+        *list = new_list;
+      }
+      else{
+        temp->next = current->next;
+        free(current);
+      }
+    }
+    temp = current;
+    current = current->next;
+  }
+}
+
+void insert_head(Node** list, int data){
+  Node *new_list = malloc(sizeof(Node));
+  new_list->data = data;
+  new_list->next = *list;
+  *list = new_list;
+}
+
+void insert_tail(Node** list, int data){
+  Node *current = *list;
+  Node *new_node = malloc(sizeof(Node));
+  new_node->data = data;
+  new_node->next = NULL;
+
+  if(list == NULL) *list = new_node;
+
+  while(current->next != NULL){
+    current = current->next;
+  }
+  current->next = new_node;
+}
+
+void print_list(Node** list){
+  Node *current = *list;
+
+  if(current == NULL) printf("No data stored in linked list\n");
+
+  else{
+    printf("The data stored in the list: { ");
+    while(current != NULL){
+      printf("%d ", current->data);
+      current = current->next;
+    }
+    printf("}\n");
+  }
+}
